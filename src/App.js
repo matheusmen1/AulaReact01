80c18e1f5e945319f47fe5d1f87d7a0e1bc56976
matheusmen1 <antonuccimatheus1@outlook.com>
2024-08-12 09:56:53 -0300

    Ententendo a estrutura de um projeto React

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {' Data/Hora em que a pagina foi carregada: ' + new Date().toLocaleDateString()}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

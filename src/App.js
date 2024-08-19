a2068715559c89a4aeefd765448a29e06c0c1457
matheusmen1 <antonuccimatheus1@outlook.com>
2024-08-19 10:06:56 -0300

    Entendendo Ciclo de Vida Aula 02

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ function App() {
   return (
     <div className="App">
       <Data texto = "Data e hora atuais: "/>
-      
+      <Data texto = "Data e hora em Londres: "/>
+      <Data texto = "Data e hora em Nova York"/>
     </div>
   );
 }
